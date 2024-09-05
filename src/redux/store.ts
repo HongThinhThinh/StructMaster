2c71996a8ec487d3f19a3c033a75bcc88c6bf526
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-09-05 21:26:58 +0700

    Config: persistent, useContext

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./features/userSlice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import { Persistor } from "redux-persist"; // Import types from redux-persist
+import { Store } from "@reduxjs/toolkit"; // Import Store type
+import rootReducer from "./rootReducer";
 
-export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+// Persist config typing
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Properly typed rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Typed store
+export const store: Store = configureStore({
+  reducer: persistedReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+// Typed persistor
+export const persistor: Persistor = persistStore(store);
